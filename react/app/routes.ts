c6267bcbddb0d4e182b0ca341897a47a508253a9
lommetack <lomme.tack@student.howest.be>
2026-03-17 14:31:29 +0100

    details

--- react/app/routes.ts
+++ react/app/routes.ts
@@ -1,3 +1,7 @@
 import { type RouteConfig, index, route} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx"), route('/meals', 'routes/meals.tsx')] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  route("meals", "routes/meals.tsx"),
+  route("meals/:mealId", "routes/meal-detail.tsx"),
+] satisfies RouteConfig;
